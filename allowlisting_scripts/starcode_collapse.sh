e96b002a27529a5ff01f45d85b61871edbddf124
Kunal Jindal <46723936+jindalk@users.noreply.github.com>
2022-08-11 16:04:13 -0500

    Update starcode_collapse.sh

--- allowlisting_scripts/starcode_collapse.sh
+++ allowlisting_scripts/starcode_collapse.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 if (($# != 2)); then
-  echo invalid number of arguments, please
+  echo invalid number of arguments, please provide distance threshold (recommended value is 4) followed by file path to the output of celltag parsing script
   exit
 fi
 
-/scratch/smlab/starcode/starcode -d $1 -t 4 -s -i $2 -o dist$1_$2
+/scratch/smlab/starcode/starcode -d $1 -t 4 -s -i $2 -o ${2:0:-4}_dist_$1.txt
 
